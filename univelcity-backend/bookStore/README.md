b983f9de880921ef573672bdbf81fb5bba31046e
Evarmedia <mishakmanuel@gmail.com>
2024-05-10 11:27:03 +0100

    update

--- univelcity-backend/bookStore/README.md
+++ univelcity-backend/bookStore/README.md
@@ -3,3 +3,9 @@ npm install express
 npm install nodemon
 npm install mongodb
 npm install mongoose
+
+mongoose.connect("mongodb+srv://mishakmanuel:ROcA1kUfF4w9pwbs@cluster0.zcwmmue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+    console.log("Connections established")
+}).catch(()=>{
+    console.log("Connetion failed");
+});
--- univelcity-backend/week6/index.js
+++ univelcity-backend/week6/index.js
@@ -1,33 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const Product = require('./models/Product.js');
 
-app.get('/', (req, res) => {
+app.use(express.json()); // middleware to use json file format
+
+
+app.get('/', (req, res) => { // default route
     res.send("Welcome to the Best Backend");
 });
 
 
-app.get('/api/data/', (req, res) => {
-    const data = [
-        {
-            name: 'Adam',
-            age: 36,
-        },
-        {
-            name: 'John',
-            age: 40,
-        },
-        {
-            name: 'Eve',
-            age: 40,
-        }
-    ]
-    res.json(data);
+app.get('/api/products', async (req, res) => { // get all products route
+    const products = await Product.find({}); // initialize the products
+    res.status(200).json(products); // get all products if status is 200
 });
 
-app.listen(3020, (req, res) => {
-    console.log('Server is listening on port 3020');
+app.post('/api/products', async (req, res) => {
+    try {
+
+        if (!req.body.name || !req.body.quantity || !req.body.price) {
+            res.status(400).json({message: "Kindly enter all required fields"});
+        }
+        else{
+            const product = await Product.create(req.body);
+            res.status(200).json(product);
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 });
+
+app.listen(3000, (req, res) => {
+    console.log('Server is listening on port 3000');
+});
+
+
 
 mongoose.connect("mongodb+srv://mishakmanuel:ROcA1kUfF4w9pwbs@cluster0.zcwmmue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
 
